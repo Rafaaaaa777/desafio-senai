7da4a3308520821d62845f5c50a4661c54c5cb57
Rafaaaaa777 <rflbnjmn@gmail.com>
2024-05-07 14:51:12 -0300

    <update>[GitActions]: Adicionado step de deploy

--- AWS-EC2.tf
+++ AWS-EC2.tf
@@ -7,7 +7,7 @@ provider "aws" {
 resource "aws_instance" "ec2_instance" {
   ami           = "ami-07caf09b362be10b8"
   instance_type = "t2.micro"
-  subnet_id     = "subnet-00e6962bae4034aeb" # ID da Subnet
+  subnet_id     = "subnet-07eb1777af2dd2e17" # ID da Subnet
   vpc_security_group_ids = ["${aws_security_group.instance_sg.id}"]
 
   key_name = "vockey"
